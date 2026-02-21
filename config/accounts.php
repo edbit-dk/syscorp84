5665c53f39c319dd2498392c011a01a64212df28
Thomas Elvin <thomas.e.s.l@outlook.dk>
2026-02-21 23:50:15 +0100

    db fix
    fixed db seeder user

--- config/accounts.php
+++ config/accounts.php
@@ -1,40 +1,37 @@
 <?php
 
 return [
-    // SRI
+    // 
     ['host_id' => 1, 'user_id' => 1],
     ['host_id' => 1, 'user_id' => 2],
     ['host_id' => 1, 'user_id' => 3],
-    // UCLA
+    // 
     ['host_id' => 2, 'user_id' => 1],
     ['host_id' => 2, 'user_id' => 2],
     ['host_id' => 2, 'user_id' => 3],
-    // UCSB
+    // 
     ['host_id' => 3, 'user_id' => 1],
     ['host_id' => 3, 'user_id' => 2],
     ['host_id' => 3, 'user_id' => 3],
-    // UCSB
+    // 
     ['host_id' => 4, 'user_id' => 1],
     ['host_id' => 4, 'user_id' => 2],
-    //DDN MIL
+    ['host_id' => 4, 'user_id' => 3],
+    //
     ['host_id' => 5, 'user_id' => 1],
     ['host_id' => 5, 'user_id' => 2],
-    //DSNET1
+    ['host_id' => 5, 'user_id' => 3],
+    //
     ['host_id' => 6, 'user_id' => 1],
     ['host_id' => 6, 'user_id' => 2],
-    //DSNET2
+    ['host_id' => 6, 'user_id' => 3],
+    //
     ['host_id' => 7, 'user_id' => 1],
     ['host_id' => 7, 'user_id' => 2],
-    //DSNET3
+    ['host_id' => 7, 'user_id' => 3],
+    //
     ['host_id' => 8, 'user_id' => 1],
     ['host_id' => 8, 'user_id' => 2],
-    //Telenet
-    ['host_id' => 9, 'user_id' => 1],
-    ['host_id' => 9, 'user_id' => 2],
-    //Poseidonet
-    ['host_id' => 10, 'user_id' => 1],
-    ['host_id' => 10, 'user_id' => 2],
-    //SPSDD
-    ['host_id' => 11, 'user_id' => 1],
-    ['host_id' => 11, 'user_id' => 2]
+    ['host_id' => 8, 'user_id' => 3],
+
 ];
--- config/users.php
+++ config/users.php
@@ -2,26 +2,37 @@
 
 return [
     [
-        'username' => 'syscorp', 
-        'email' => 'scs@syscorp', 
+        'username' => 'ADMIN', 
+        'email' => 'ADMIN@FALLHACK', 
         'password' => random_pass(),
         'code' => access_code(),
-        'fullname' => 'The SysCorp Syndicate',
+        'fullname' => 'FALLHACK ADMIN',
         'role' => 'ADMIN',
         'is_admin' => 1,
         'level_id' => 6,
         'credits' => 100
     ],
     [
-        'username' => 'user', 
-        'email' => 'user@syscorp', 
+        'username' => 'OPERATOR', 
+        'email' => 'OPERATOR@FALLHACK', 
         'password' => random_pass(),
         'code' => access_code(),
-        'fullname' => 'The SysCorp Syndicate',
-        'role' => 'ADMIN',
+        'fullname' => 'FALLHACK OPERATOR',
+        'role' => 'OPERATOR',
         'is_admin' => 1,
-        'level_id' => 6,
-        'credits' => 100
+        'level_id' => 3,
+        'credits' => 50
+    ],
+    [
+        'username' => 'USER', 
+        'email' => 'USER@FALLHACK', 
+        'password' => random_pass(),
+        'code' => access_code(),
+        'fullname' => 'FALLHACK USER',
+        'role' => 'USER',
+        'is_admin' => 0,
+        'level_id' => 1,
+        'credits' => 10
     ],
     
 ];
